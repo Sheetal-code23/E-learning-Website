4e2330bc68b738fd5909ae7b3baa1dfe7047d168
Sheetal-code23 <jsheetal756@gmail.com>
2025-04-08 17:15:40 +0530

    Create landingpage.js

---
+++ landingpage.js
@@ -0,0 +1,15 @@
+document.querySelector('.login').addEventListener('click', function() {
+    alert('Login button clicked!');
+});
+
+document.querySelector('.register').addEventListener('click', function() {
+    alert('Register button clicked!');
+});
+
+document.querySelector('.start-learning').addEventListener('click', function() {
+    alert('Start Learning button clicked!');
+});
+
+document.querySelector('.watch-video button').addEventListener('click', function() {
+    alert('Watch Video button clicked!');
+});
